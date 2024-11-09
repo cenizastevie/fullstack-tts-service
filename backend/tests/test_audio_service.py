92582fe857b5f9307a27963824adb7303cc7c217
ceniza.sjs <cenizastevie@gmail.com>
2024-11-09 15:33:29 +0800

    Implement test for uploading in the presigned url now.

--- backend/tests/test_audio_service.py
+++ backend/tests/test_audio_service.py
@@ -1,9 +1,13 @@
 from fastapi.testclient import TestClient
+
+import boto3
 import os
 import sys
+import requests
 sys.path.append(os.path.join(os.path.dirname(__file__), '../app'))
 
 from app.main import app
+from app.config import settings
 
 client = TestClient(app)
 
@@ -31,3 +35,28 @@ def test_get_presigned_url_invalid_filename():
     json_response = response.json() 
     assert json_response["detail"][0]["msg"] == "String should have at least 5 characters"
     assert json_response["detail"][0]["type"] == "string_too_short"
+
+def test_upload_file_to_presigned_url():
+    # Step 1: Generate the presigned URL
+    response = client.post("/v1/audio-service/presigned-url", json={"filename": "test_audio.mp3"})
+    assert response.status_code == 200
+    presigned_url_data = response.json()
+
+    # Step 2: Upload the file using the presigned URL
+    files = {'file': open('test_files/test_audio.mp3', 'rb')}
+    response = requests.post(presigned_url_data['url'], data=presigned_url_data['fields'], files=files)
+
+    # Check if the file upload was successful
+    assert response.status_code == 204
+
+    # Step 3: Verify the file is in the S3 bucket
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=settings.aws_access_key_id,
+        aws_secret_access_key=settings.aws_secret_access_key,
+        endpoint_url=f"http://{os.getenv('LOCALSTACK_HOSTNAME')}:{os.getenv('LOCALSTACK_PORT')}"
+    )
+    response = s3_client.list_objects_v2(Bucket=settings.audio_bucket)
+    assert 'Contents' in response
+    filenames = [obj['Key'] for obj in response['Contents']]
+    assert "test_audio.mp3" in filenames
