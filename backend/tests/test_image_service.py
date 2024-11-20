cfcf6e6bb4378b1554b3b22dbbdc8937638f621b
ceniza.sjs <cenizastevie@gmail.com>
2024-11-20 17:18:54 +0800

    Remove presigned url tests.

--- backend/tests/test_image_service.py
+++ backend/tests/test_image_service.py
@@ -33,66 +33,6 @@ def test_image_service_health_check():
     response = client.get("/v1/image-service/health-check")
     assert response.status_code == 200
 
-def test_get_presigned_url():
-    response = client.post("/v1/image-service/presigned-url", json={"filename": "test.jpeg"})
-    assert response.status_code == 200
-    json_response = response.json()
-    assert "url" in json_response
-    assert "fields" in json_response
-
-def test_get_presigned_url_missing_filename():
-    response = client.post("/v1/image-service/presigned-url", json={})
-    assert response.status_code == 422  # Unprocessable Entity
-    json_response = response.json()
-    assert json_response["detail"][0]["msg"] == "Field required"
-    assert json_response["detail"][0]["type"] == "missing"
-
-def test_get_presigned_url_invalid_filename():
-    response = client.post("/v1/image-service/presigned-url", json={"filename": ""})
-    assert response.status_code == 422  # Unprocessable Entity
-    json_response = response.json() 
-    assert json_response["detail"][0]["msg"] == "String should have at least 5 characters"
-    assert json_response["detail"][0]["type"] == "string_too_short"
-
-def test_upload_file_to_presigned_url():
-    # Step 1: Generate the presigned URL
-    response = client.post("/v1/image-service/presigned-url", json={"filename": "test.jpeg"})
-    assert response.status_code == 200
-    presigned_url_data = response.json()
-    print(presigned_url_data)
-    # Step 2: Upload the file using the presigned URL
-    file_path = os.path.join(os.path.dirname(__file__), 'test_files', 'test.jpeg')
-    files = {'file': open(file_path, 'rb')}
-    response = requests.post(presigned_url_data['url'], data=presigned_url_data['fields'], files=files)
-    # Check if the file upload was successful
-    assert response.status_code == 204
-    
-    # Step 3: Verify the file is in the S3 bucket
-    s3_client = boto3.client(
-        "s3",
-        endpoint_url=f"http://{os.getenv('LOCALSTACK_HOSTNAME')}:{os.getenv('LOCALSTACK_PORT')}"
-    )
-    response = s3_client.list_objects_v2(Bucket=settings.image_bucket)
-    assert 'Contents' in response
-    filenames = [obj['Key'] for obj in response['Contents']]
-    assert "test.jpeg" in filenames
-
-    # Step 4: Download the image from S3 and verify preprocessing
-    response = s3_client.get_object(Bucket=settings.image_bucket, Key="test.jpeg")
-    image_data = response['Body'].read()
-    image = Image.open(BytesIO(image_data))
-
-    # Verify the image is grayscale
-    assert image.mode == "L"
-
-    # Verify the image dimensions
-    assert image.size == (256, 256)
-
-    # Verify the pixel value range
-    image_np = np.array(image)
-    assert image_np.min() >= 0
-    assert image_np.max() <= 255
-
 def test_upload_image_with_metadata():
     file_path = os.path.join(os.path.dirname(__file__), 'test_files', 'test.jpeg')
     with open(file_path, 'rb') as file:
